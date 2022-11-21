eeb11206fabf7aae50f62217e417126d1220fe45
nkr <nkgithubnarkuziev108@gmail.com>
2022-11-22 02:25:31 +0500

    updated print.rs

--- src/print.rs
+++ src/print.rs
@@ -1,26 +1,14 @@
-pub mod print_fn {
-	#![allow(warnings)]
-
+pub mod print_fn
+{
 	// PACKAGES
 	use std::fs::File;
 	use std::error::Error;
 	use std::io::{prelude::*, BufReader};
 
-	extern crate rusqlite;
-	use rusqlite::{params, Connection, Result, NO_PARAMS};
-
 	use serde::{Deserialize, Serialize};
 	use serde_yaml::{self};
 	// PACKAGES
 
-	#[derive(Debug)]
-	struct FileStr {
-		id: i64,
-		file_path: String,
-		save_name: String,
-		saved_date: String,
-		saved_time: String
-	}
 
 	#[derive(Debug, Serialize, Deserialize)]
 	struct Config {
@@ -45,7 +33,7 @@ pub mod print_fn {
 	}
 
 
-	fn show_log() -> Result<(), std::io::Error> {
+	fn show_log() -> std::io::Result<()> {
 		let file = File::open("rustix/log.txt")?;
 		let reader = BufReader::new(file);
 		for line in reader.lines() { println!("{}", line?); }
@@ -63,27 +51,16 @@ pub mod print_fn {
 	}
 
 
-	fn print_db() -> Result<(), Box<dyn Error>> {
-		let conn = Connection::open("rustix/storage.db3")?;
-		let mut stmt = conn.prepare("SELECT * FROM main")?;
+	fn print_db() {
+		let SAVES_BASE = crate::database::get::start().unwrap();
+		let mut id : i64 = 1;
 
-		let mut base = stmt.query_map(NO_PARAMS, |row| {
-			Ok(FileStr {
-				id: row.get(0)?,
-				file_path: row.get(1)?, save_name: row.get(2)?,
-				saved_date: row.get(3)?, saved_time: row.get(4)?,
-			})
-		})?;
-		
-		let mut new_base: Vec<FileStr> = Vec::new();
-		for one in base.into_iter() { new_base.push(one.unwrap()); }
-
-		for x in new_base.into_iter() {
+		for x in SAVES_BASE.into_iter() {
 			println!("{}. {}\n   - path\n      {}\n   - saved\n      {}\n      {}\n",
-			x.id, x.save_name, x.file_path, x.saved_date, x.saved_time);
-		}
+			id, x[1], x[0], x[2], x[3]);
 
-		Ok(())
+			id += 1;
+		}
 	}
 
 
